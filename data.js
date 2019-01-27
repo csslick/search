0325d086423a66482c5faccec7e621ab6269f787
csslick <digitone@naver.com>
2019-01-27 21:21:19 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -7,6 +7,12 @@ var data = [
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.5478323142397!2d127.1182131509609!3d37.49499537971239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca58f4ae6d083%3A0x6f64a0b74c741c40!2z7KOg7Iqk65ah67O27J206rCA65297KCQ!5e0!3m2!1sko!2skr!4v1548423529327'
   },
   {
+    name: '가산디지털점',
+    addr: '서울시 금천구 가산동 50-3 대륭포스로 6차 1층 102호',
+    url:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.9135711915605!2d126.89415467946387!3d37.48636588062685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9e3b6a692ded%3A0x4540cf91992bc50e!2z7KOg7Iqk65ah67O27J20KOq1rOuhnOuUlOyngO2EuDLtmLjsoJAp!5e0!3m2!1sko!2skr!4v1548499054146'
+  },
+  {
     name: '송도신도시점',
     addr: '인천광역시 연수구 송도동 3-6 아크리아 1 101호',
     url:
